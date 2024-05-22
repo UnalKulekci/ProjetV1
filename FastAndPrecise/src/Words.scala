ca9f2d45d47be9689359b6345f77d0f4b78c5fc7
fylis <filip.siliwoniuk@gmail.com>
2024-05-22 14:19:38 +0200

    Change words import from .csv file

--- FastAndPrecise/src/Words.scala
+++ FastAndPrecise/src/Words.scala
@@ -1,32 +1,52 @@
 import java.io.{BufferedReader, FileNotFoundException, FileReader}
 import scala.collection.mutable.ArrayBuffer
+import scala.io.Source
+
 
 object Words {
 
-  val fileName: String = "data/french_common.csv"
-  val wordsBuffer: ArrayBuffer[String] = ArrayBuffer[String]()
-
-  try {
-    val fr = new FileReader(fileName)
-    val inputReader = new BufferedReader(fr)
-
-    var line = inputReader.readLine()
-    while (line != null) {
-      val words = line.split(";")
-      words.foreach(word => wordsBuffer += word.strip())
-      line = inputReader.readLine()
+  def noAccent(word: String): String = {
+    var res: Array[Char] = word.toCharArray
+    for (i <- 0 until res.length) {
+      if(res(i) == 'é' || res(i) == 'è' || res(i) == 'ë' || res(i) == 'ê' || res(i) == 'ę'){
+        res(i) = 'e'
+      }
+      if(res(i) == 'à' || res(i) == 'â' || res(i) == 'ä' || res(i) == 'ą'){
+        res(i) = 'a'
+      }
+      if(res(i) == 'ù' ||res(i) == 'ü' ||res(i) == 'ú' || res(i) == 'û'){
+        res(i) = 'u'
+      }
+      if(res(i) == 'ï' ||res(i) == 'î'){
+        res(i) = 'i'
+      }
+      if(res(i) == 'ô'){
+        res(i) = 'o'
+      }
+      if(res(i) == 'ç'){
+        res(i) = 'c'
+      }
     }
-
-    inputReader.close()
-  } catch {
-    case e: FileNotFoundException =>
-      println("File not found!")
-      e.printStackTrace()
-    case e: Exception =>
-      println(s"Something bad happened during read ${e.getMessage}")
+    res.mkString("")
   }
 
-  def getWords: ArrayBuffer[String] = {
-    wordsBuffer
+  def GetFromFile(fileName: String): Array[String] = {
+    try {
+      var res: Array[String] = Source.fromFile(fileName).getLines().toArray
+      for (i <- 0 until res.length) {
+        res(i) = noAccent(res(i).split(";")(0))
+      }
+      return res
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+        return Array.empty
+    }
   }
+
+
+}
+
+object test extends App {
+  println(Words.GetFromFile("data/french_common.csv").mkString("\n"))
 }
