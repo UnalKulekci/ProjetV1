a215e5afd6e2526b805176304b7fad01a6552556
ted <76978205+UnalKulekci@users.noreply.github.com>
2024-05-28 21:44:40 +0200

    onKeyDown method works corretcly in test code

--- FastAndPrecise/src/GamesDevPart.scala
+++ FastAndPrecise/src/GamesDevPart.scala
@@ -2,6 +2,8 @@ import ch.hevs.gdx2d.desktop.PortableApplication
 import ch.hevs.gdx2d.lib.GdxGraphics
 import com.badlogic.gdx.graphics.Color
 
+import scala.collection.mutable.ArrayBuffer
+
 class GamesDevPart extends PortableApplication() {
 
   override def onInit(): Unit = {
@@ -9,32 +11,35 @@ class GamesDevPart extends PortableApplication() {
     setTitle("Games Test")
   }
 
-  var posx: Float = 100f
-  var posy: Float = 100f
-
   override def onGraphicRender(g: GdxGraphics): Unit = {
     g.clear()
     g.setBackgroundColor(Color.WHITE)
     g.setColor(Color.BLACK)
     //Gdx.input.isKeyPressed()
+    var posx: Float = 100f
+    var posy: Float = 100f
 
-    g.drawString(posx, posy, words)
+    for( i <- arrSorted.indices){
+      g.drawString(posx, posy, arrSorted(i))
+      posx += 50
+      posy += 100
+    }
   }
 
-  var words : String = "sion"
+  var arrSorted: ArrayBuffer[String] = ArrayBuffer[String]("unal", "filip", "sion", "aba").sortWith(_ < _)
   override def onKeyDown(keycode: Int): Unit = {
     val chr = (keycode + 68).toChar
+    var selectedWord: String = ""
 
-    for(i <- 0 until words.length) {
-      if(chr == words(i)) {
-        words = words.substring(i+1)
-        println(words)
-        return
+    for (i <- 0 until arrSorted.length) {
+      if (arrSorted(i).startsWith(chr.toString)) {
+        selectedWord = arrSorted(i)
+        arrSorted(i) = arrSorted(i).substring(1)
       }
     }
   }
 
-}
+} // End of the class
 
 object gtest extends App {
   val game: GamesDevPart = new GamesDevPart()
