7e251d7b91de0d8e608600297be038701338636d
ted <76978205+UnalKulekci@users.noreply.github.com>
2024-05-28 16:06:58 +0200

    onKeyDown method added but it is not working properly at the moment

--- FastAndPrecise/src/Games.scala
+++ FastAndPrecise/src/Games.scala
@@ -4,7 +4,6 @@ import ch.hevs.gdx2d.desktop.PortableApplication
 import ch.hevs.gdx2d.lib.GdxGraphics
 import com.badlogic.gdx.{Gdx, InputAdapter}
 import com.badlogic.gdx.graphics.Color
-
 import java.util.{Timer, TimerTask}
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
@@ -12,7 +11,7 @@ import scala.util.Random
 
 class Games extends PortableApplication  {
 
-  val words: ArrayBuffer[String] = new Words().getWords()
+  val words: ArrayBuffer[String] = Words.getWords()
   var wordIndex = 0
 
   val fallingWords = new ArrayBuffer[WordPosition]()
@@ -20,7 +19,6 @@ class Games extends PortableApplication  {
 
   var img: BitmapImage = _
 
-  var key = new Key()
 
    // Rastgele koordinatlar için yardımcı fonksiyon
   def getRandomPosition(maxWidth: Float, maxHeight: Float): (Float, Float) = {
@@ -30,34 +28,21 @@ class Games extends PortableApplication  {
   }
 
 
-  def checkWords(g: GdxGraphics, key: Char): Unit = {
+  override def onKeyDown(keycode: Int): Unit = {
+    val chr = (keycode + 68).toChar
 
-    if (fallingWords.nonEmpty) {
-      val wordInDisplay = fallingWords(0).word
-
-      if (wordInDisplay.nonEmpty && wordInDisplay(0) == key) {
-        g.drawCircle(50f, 50f, 60f)
+    for (i <- 0 until words.length) {
+      if (chr == words(i)) {
+        fallingWords(0).word = fallingWords(0).word.substring(i + 1)
+        println(words)
+        return
       }
     }
   }
 
+
   def levelOne(): ArrayBuffer[String] = {
-    words.take(2)
-  }
-
-  def levelTwo(): ArrayBuffer[String] = {
-    words.take(11)
-  }
-
-  def levelThree(): ArrayBuffer[String] =
-    words.take(21)
-
-
-  def checkWords(g:GdxGraphics): Unit = {
-   var wordInDisplay = fallingWords(0).word
-    if(wordInDisplay(0) == key) {
-      g.drawCircle(50f,50f,60f)
-    }
+    words.take(1)
   }
 
   // onInit method
@@ -67,29 +52,27 @@ class Games extends PortableApplication  {
 
     img = new BitmapImage("data/rocket.png")
 
-    // Her saniye yeni bir kelime eklemek için timer ayarı
+
     timer.scheduleAtFixedRate(new TimerTask {
       override def run(): Unit = {
 
         var wordsLevelOne = levelOne()
-        var wordsLevelTwo = levelTwo()
-        var wordsLevelThere = levelThree()
 
         if (wordIndex < wordsLevelOne.length) {
           val (x, y) = getRandomPosition(getWindowWidth.toFloat, getWindowHeight.toFloat)
-          fallingWords += WordPosition(words(wordIndex), x, y) // Yeni kelimeyi ekler
+          fallingWords += WordPosition(words(wordIndex), x, y)
           wordIndex += 1
         } else {
-          timer.cancel() // Tüm kelimeler eklendiğinde timer'ı durdur
+          timer.cancel()
         }
-        // Ekranı yeniden çizdir
+
         Gdx.app.postRunnable(new Runnable {
           override def run(): Unit = {
             forceRedraw()
           }
         })
       }
-    }, 0, 1000) // 1 saniyelik aralıklarla kelime ekler
+    }, 0, 1000)
   }
 
   // onGraphicRender method
@@ -101,6 +84,7 @@ class Games extends PortableApplication  {
     val screenHeight = getWindowHeight.toFloat
     g.setColor(Color.BLACK)
 
+
     // Update and draw falling words
     for (wordPos <- fallingWords) {
       wordPos.y -= 1 // Her çerçevede y pozisyonunu azalt
@@ -111,9 +95,10 @@ class Games extends PortableApplication  {
     fallingWords --= fallingWords.filter(_.y < 0)
   }
 
-  // Bu metot ekranda yeniden çizimi zorlamak için kullanılabilir.
+
   def forceRedraw(): Unit = {
-    // Ekranda bir yeniden çizim işlemi başlatır.
     Gdx.graphics.requestRendering()
   }
-}
+
+
+} // End of the Games Class
