0b28daca29e3d1c8c27dc19d65073b7869846f51
fylis <filip.siliwoniuk@gmail.com>
2024-06-06 00:02:41 +0200

    [ADDED] returnCurrentIdx function: Returns of the word that starts with the letter typed on the keyboard and which is at the bottom. [UPDATED] deleted unnecessary commentaries as well as deleted test object to test the function -> it is right now in `Main.scala`

--- FastAndPrecise/src/Main.scala
+++ FastAndPrecise/src/Main.scala
@@ -1,16 +1,7 @@
 import com.badlogic.gdx.Gdx
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    //println("Hello world!")
-
-  }
-  val words = new Words().getWords
-  println(words.length)
-  var g : Games = new Games
-
-
-
+object Main extends App {
+  val fast_and_precise_game: Mixed = new Mixed()
 }
 
 
--- FastAndPrecise/src/Mixed.scala
+++ FastAndPrecise/src/Mixed.scala
@@ -11,48 +11,81 @@ import scala.util.Random
 /*  TODO ::
      AT GAME OVER, THE GAME STOPS AND RESTARTS IF THE USER CLICKS YES -- restart will added
      YOU NEED TO INCREASE THE FONT OF THE WORDS ON THE SCREEN -- ??
-     INCREASE THE RATE AT WHICH THE WORDS FALL WITH EACH ROUND -- THAT'S IT, IF PHILIP SAYS OKAY.
  */
 
 class Mixed extends PortableApplication(1920, 1080) {
 
-  val w: ArrayBuffer[ArrayBuffer[String]] = Words.createRoundArray(Words.getWords().toArray)
-  var arrSorted: ArrayBuffer[String] = ArrayBuffer.empty[String]
-  val arrSortedLength: ArrayBuffer[Int] = ArrayBuffer.empty[Int]
-  var currentWordIndex = -1
-  var roundCounter: Int = 0
-  var fallingWords = new ArrayBuffer[WordPosition]()
+  private val w: ArrayBuffer[ArrayBuffer[String]] = Words.createRoundArray(Words.getWords().toArray)
+  private var arrSorted: ArrayBuffer[String] = ArrayBuffer.empty[String]
+  private val arrSortedLength: ArrayBuffer[Int] = ArrayBuffer.empty[Int]
+  private var currentWordIndex = -1
+  private var roundCounter: Int = 0
+  private val fallingWords = new ArrayBuffer[WordPosition]()
+  private var isGameOver = false // Game over state
+  private var scoresCounter: Float = 0f
+  private var idxTimer: Int = 0
+  private var secondTimer: Int = 0
 
-  var isGameOver = false // Game over state
-
-
-  override def onInit(): Unit = {
-    println("Game initialized")
-    setTitle("MIXED")
+  // Word select after pressing a key depending on the y position as well as the order of letters
+  private def returnCurrentIdx(a: ArrayBuffer[WordPosition], c: Char): Int = {
+    var res: Int = 0
+    var min: Int = 0
+    var x: ArrayBuffer[WordPosition] = ArrayBuffer.empty[WordPosition]
+    for (i <- a.indices) {
+      if (a(i).word.startsWith(c.toString)) {
+        x.append(a(i))
+      }
+    }
+    println(x.mkString(","))
+    x = x.sortBy(_.word)
+    if (x.length == 1) {
+      return a.indexOf(x(0))
+    } else if (x.isEmpty) {
+      return -1
+    } else {
+      min = 0
+      for (i <- x.indices) {
+        if (x(min).y > x(i).y) {
+          min = i
+        }
+      }
+      return a.indexOf(x(min))
+    }
+    -1
   }
 
-  def getRandomPosition(maxWidth: Float, maxHeight: Float): (Float, Float) = {
-    val x = Random.nextFloat() * maxWidth
+  private def getRandomPosition(maxWidth: Float, maxHeight: Float): (Float, Float) = {
+    val x = 100 + Random.nextFloat() * (maxWidth - 200)
     val y = maxHeight
     (x, y)
   }
 
-  var scoresCounter : Float = 0f
-  var fallingVitesse : Float = 0.5f // to increase falling vitesse each round
+  override def onInit(): Unit = {
+    println(Console.MAGENTA + "FAST & PRECISE" + Console.RESET + " 2024 " + Console.RESET + "by " + Console.BLUE + "KÜLEKÇI ÜNAL" + Console.RESET + "and" + Console.YELLOW + " SILIWONIUK FILIP" + Console.RESET)
+    setTitle("FAST & PRECISE")
+  }
+
 
   override def onGraphicRender(g: GdxGraphics): Unit = {
     g.clear()
+    if (!isGameOver) {
+      idxTimer += 1
+      if (idxTimer % 60 == 0) {
+        secondTimer += 1
+      }
+    }
+    g.drawString(100f, 150f, s"Round : ${roundCounter + 1}          Time : ${secondTimer}")
     g.setBackgroundColor(Color.WHITE)
 
     // Set the scores
-    g.drawString(100f,100f,s" Total Scores : ${scoresCounter}")
+    g.drawString(100f, 100f, s" Total Scores : ${scoresCounter}")
 
 
     if (roundCounter < w.length) {
       arrSorted = w(roundCounter)
       if (fallingWords.length < arrSorted.length) {
         for (i <- 0 until arrSorted.length) {
-          val (x, y) = getRandomPosition(getWindowWidth.toFloat, getWindowHeight.toFloat)
+          val (x, y) = getRandomPosition(getWindowWidth.toFloat, getWindowHeight.toFloat + Random.between(0, 120))
           fallingWords.append(WordPosition(arrSorted(i), x, y))
         }
       }
@@ -65,7 +98,7 @@ class Mixed extends PortableApplication(1920, 1080) {
 
     // Update and draw falling words
     for (i: Int <- 0 until fallingWords.length) {
-      fallingWords(i).y -= fallingVitesse
+      fallingWords(i).y -= 1 // -60 pixels/s
 
       if (arrSortedLength(i) != fallingWords(i).word.length) {
         g.setColor(new Color(212, 0, 103, 255))
@@ -73,11 +106,13 @@ class Mixed extends PortableApplication(1920, 1080) {
         g.setColor(Color.BLACK)
       }
 
+      // Drawing words on the screen
       g.drawString(fallingWords(i).x, fallingWords(i).y, fallingWords(i).word)
+
       g.setColor(Color.BLACK)
 
-      if(fallingWords(i).y < 0) {
-        g.drawString(g.getScreenWidth/2,g.getScreenHeight/2,"G  A  M  E  O  V  E  R")
+      if (fallingWords(i).y < 0) {
+        g.drawString(g.getScreenWidth / 2, g.getScreenHeight / 2, "G  A  M  E  O  V  E  R")
         isGameOver = true
       }
     }
@@ -86,42 +121,29 @@ class Mixed extends PortableApplication(1920, 1080) {
   }
 
   override def onKeyDown(keycode: Int): Unit = {
-    if (isGameOver) return
+    if (isGameOver) {
+      return
+    }
 
     val chr = (keycode + 68).toChar
     if (currentWordIndex == -1) {
-      for (idx <- arrSorted.indices) {
-        if (fallingWords(idx).word.startsWith(chr.toString)) {
-          currentWordIndex = idx
-        }
-      }
+      currentWordIndex = returnCurrentIdx(fallingWords, chr)
     }
 
     if (currentWordIndex != -1 && fallingWords(currentWordIndex).word.startsWith(chr.toString)) {
-      //      arrSorted(currentWordIndex) = arrSorted(currentWordIndex).substring(1)
       fallingWords(currentWordIndex).word = fallingWords(currentWordIndex).word.substring(1)
       if (fallingWords(currentWordIndex).word.isEmpty) {
+        scoresCounter += (roundCounter + 1) * arrSortedLength(currentWordIndex)
         arrSorted.remove(currentWordIndex)
-        scoresCounter += (roundCounter+1) * arrSortedLength(currentWordIndex)
         fallingWords.remove(currentWordIndex)
         arrSortedLength.remove(currentWordIndex)
         currentWordIndex = -1
         if (fallingWords.isEmpty) {
           roundCounter += 1
-          fallingVitesse += 0.3f
-          //roundPenderation += 1 // --
           println("Round is over you can pass the other...")
         }
       }
     }
   }
-}
-
-
-object test extends App {
-  val g1: Mixed = new Mixed()
-
-
-
 }
 
