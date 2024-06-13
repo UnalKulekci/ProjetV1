0e583f1608e019874d2682993f1da624b3599162
fylis <filip.siliwoniuk@gmail.com>
2024-06-13 02:06:12 +0200

    [ADDED] This function enables to create fonts from the Star Jedi font

---
+++ FastAndPrecise/src/SpecialFont.scala
@@ -0,0 +1,24 @@
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator
+import com.badlogic.gdx.graphics.g2d.{Batch, BitmapFont, SpriteBatch}
+
+object SpecialFont {
+  // FONT
+  private val starjedi = Gdx.files.internal("data/font/Starjedi.ttf")
+  private val parameter = new FreeTypeFontGenerator.FreeTypeFontParameter
+  private val generator = new FreeTypeFontGenerator(starjedi)
+
+  def create(outside_color: String, inside_color: String, size: Int): BitmapFont = {
+    parameter.size = generator.scaleForPixelHeight(size)
+    parameter.color = Color.valueOf(inside_color)
+    parameter.borderColor = Color.valueOf(outside_color)
+    parameter.borderWidth = 3
+    return generator.generateFont(parameter)
+  }
+
+  def generate(): Unit = {
+    generator.dispose()
+  }
+
+}
